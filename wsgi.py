ff31e6641f91c58c3deb1afdc6ee3ae1cb99db1d
Alexis <al.dacosta@f5.com>
2020-07-27 22:20:54 +0200

    exportS

--- wsgi.py
+++ wsgi.py
@@ -5,12 +5,10 @@ from flasgger import Swagger
 # -------------- API --------------
 # listener
 application = Flask(__name__)
-"""
 application.config['SWAGGER'] = {
     'title': 'WebMap F5',
     'openapi': '3.0.2'
 }
-"""
 api = Api(application)
 swagger = Swagger(application)
 
